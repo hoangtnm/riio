7bd46ae3d5027f6a0d9411c64be955d39d37f9fb
hoangtnm <minhhoangtrannhat@gmail.com>
2019-11-27 13:39:54 +0700

    fix routing

--- riio/urls.py
+++ riio/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.urls import include
 from django.urls import path
 
 urlpatterns = [
@@ -21,5 +22,5 @@ urlpatterns = [
     path('', include('upload.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
