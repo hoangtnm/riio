8bc3357e4debad8163f9fce1596790db18eaa30d
hoangtnm <minhhoangtrannhat@gmail.com>
2019-12-09 21:38:04 +0700

    BE4: fix urls

--- ai/urls.py
+++ ai/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'ai'
 urlpatterns = [
     path('', views.home, name="homepage"),
-    path('upload/', views.upload_file, name='upload_file'),
-    path('history/', views.display_history, name='display_history')
+    path('upload/', views.upload, name='upload_file'),
+    path('history/', views.history, name='display_history')
 ]
